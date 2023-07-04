c895e155645c23acd5d06edeafc8e043480c9e24
Toreno96 <toreno96@gmail.com>
2023-07-04 20:42:10 +0200

    Add a template for bash script with basic options
    Based on the work done while implementing the `bin/brew-outdated`
    script in b7d34406b17c0c26c5653942ac0a314599f74d2f.

---
+++ templates/script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+VERSION=0.0.1
+
+USAGE=$(cat << EOF
+Usage: $(basename "${0}") [-hv]
+
+Placeholder title.
+
+Placeholder description.
+EOF
+)
+
+while getopts 'hv' option; do
+    case "${option}" in
+        'h')
+            echo "${USAGE}"
+            exit
+            ;;
+        'v')
+            echo "${VERSION}"
+            exit
+            ;;
+        *)
+            echo "${USAGE}"
+            exit 1
+            ;;
+    esac
+done
+
+dependencies_available() {
+    (command -v brew &>/dev/null) && (command -v semver &>/dev/null)
+}
+
+main() {
+}
+
+if dependencies_available; then
+    main
+fi
+
+# CHANGELOG
+#
+# All notable changes to this project will be documented in this file.
+#
+# The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+# and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+#
+# [0.0.1] - yyyy-mm-dd
+#
+# ADDED
+#
+# - Initial version of this script.
